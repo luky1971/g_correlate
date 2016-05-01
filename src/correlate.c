1376136245ec44a76d7688ed1058bec97759504c
Ahnaf Siddiqui <luky1971@users.noreply.github.com>
2016-05-01 18:51:12 -0400

    Getting topology stuff

--- src/correlate.c
+++ src/correlate.c
@@ -10,83 +10,39 @@
  */
 
 #include "correlate.h"
-#include "smalloc.h"
 #include "gkut_io.h"
 #include "gkut_log.h"
 
+#include "mtop_util.h"
+#include "smalloc.h"
+
 void get_corr_vecs(const char *top_fname) {
+	t_topology top;
+
 	switch(fn2ftp(top_fname)) {
 		case efGRO:
-			{
-				t_topology top;
-
 				gk_read_top_gro(top_fname, &top);
-				/*
-				if(top.idef.ntypes > 0) {
-					ndih = top.idef.il[F_PDIHS].nr;
-
-					read_traj(fnames[eTRAJ1], &x1, &nframes, &natoms, oenv);
-					read_traj(fnames[eTRAJ2], &x2, &nframes, &natoms, oenv);
-
-					ilist2svm_probs(top.idef.il, x1, x2, nframes, &probs);
-
-					snew(models, ndih);
-					train_traj(probs, ndih, gamma, c, parallel, models);
-
-					// Construct eta_dih struct
-					eta_dih->ndih = ndih;
-
-					snew(eta_dih->atoms, ndih * 4);
-					int i;
-					for(i = 0; i < ndih; ++i) {
-						eta_dih->atoms[4*i] = top.idef.il[F_PDIHS].iatoms[i*5+1];
-						eta_dih->atoms[4*i+1] = top.idef.il[F_PDIHS].iatoms[i*5+2];
-						eta_dih->atoms[4*i+2] = top.idef.il[F_PDIHS].iatoms[i*5+3];
-						eta_dih->atoms[4*i+3] = top.idef.il[F_PDIHS].iatoms[i*5+4];
-					}
-				}
-				else {
-					print_log("No interaction information found in %s. Skipping dihedral eta.\n", fnames[eTOP1]);
-					success = FALSE;
-				}*/
-
-				gk_free_topology(&top);
-			}
 			break;
 		case efTPR:
 			{
 				gmx_mtop_t mtop;
-
 				gk_read_top_tpr(top_fname, &mtop);
-				/*
-				ndih = mtop.moltype->ilist[F_PDIHS].nr;
-
-				read_traj(fnames[eTRAJ1], &x1, &nframes, &natoms, oenv);
-				read_traj(fnames[eTRAJ2], &x2, &nframes, &natoms, oenv);
-
-				ilist2svm_probs(mtop.moltype->ilist, x1, x2, nframes, &probs);
-
-				snew(models, ndih);
-				train_traj(probs, ndih, gamma, c, parallel, models);
-
-				// Construct eta_dih struct
-				eta_dih->ndih = ndih;
-
-				snew(eta_dih->atoms, ndih * 4);
-				int i;
-				for(i = 0; i < ndih; ++i) {
-					eta_dih->atoms[4*i] = mtop.moltype->ilist[F_PDIHS].iatoms[i*5+1];
-					eta_dih->atoms[4*i+1] = mtop.moltype->ilist[F_PDIHS].iatoms[i*5+2];
-					eta_dih->atoms[4*i+2] = mtop.moltype->ilist[F_PDIHS].iatoms[i*5+3];
-					eta_dih->atoms[4*i+3] = mtop.moltype->ilist[F_PDIHS].iatoms[i*5+4];
-				}*/
-
-				gk_free_mtop(&mtop);
+				top = gmx_mtop_t_to_t_topology(&mtop);
 			}
 			break;
 		default:
 			gk_log_fatal(FARGS, "%s is not a supported filetype for topology information!\n", top_fname);
 	}
+
+	if(top.idef.ntypes <= 0) {
+		gk_free_topology(&top);
+		gk_log_fatal(FARGS, "No interaction information found in %s!\n", top_fname);
+	}
+
+	// TODO
+
+	// Free topology data
+	gk_free_topology(&top);
 }
 
 void calc_ac(const char *fnames[], output_env_t *oenv, struct corr_dat_t *corr, unsigned long flags) {
