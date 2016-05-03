91ed96f9e9fae5928ebaba455ae28ab1d8f5837b
Ahnaf Siddiqui <luky1971@users.noreply.github.com>
2016-05-03 16:35:18 -0400

    Printing bonds

--- src/correlate.c
+++ src/correlate.c
@@ -40,12 +40,34 @@ void get_corr_vecs(const char *top_fname) {
     }
 
     // DEBUG
+    // Print atom names and atom numbers
+    /*
     for(int i = 0; i < top.atoms.nr; ++i) {
         printf("%d: %s: %d\n", i, 
             *(top.atoms.atomname[i]), 
             top.atoms.atom[i].atomnumber);
     }
+    */
 
+    // DEBUG
+    // Print list of bonds
+    for(int f = 0; f < F_NRE; ++f) {
+        printf("Interactions array for interaction type %d has %d elements.\n", f, top.idef.il[f].nr);
+    }
+    
+    /*
+    t_ilist bonds = top.idef.il[F_BONDS];
+    printf("\nInteractions array for BONDS has %d elements.\n", bonds.nr);
+    for(int i = 0; i < bonds.nr; i+=3) {
+        printf("Bond %d, type %d: %d %d\n", i/3, bonds.iatoms[i], bonds.iatoms[i+1], bonds.iatoms[i+2]);
+    }
+
+    bonds = top.idef.il[F_G96BONDS];
+    printf("\nInteractions array for G96BONDS has %d elements.\n", bonds.nr);
+    for(int i = 0; i < bonds.nr; i+=3) {
+        printf("Bond %d, type %d: %d %d\n", i/3, bonds.iatoms[i], bonds.iatoms[i+1], bonds.iatoms[i+2]);
+    }*/
+    
     // Free topology data
     gk_free_topology(&top);
 }
