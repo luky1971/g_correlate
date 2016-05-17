4f14070c74ac27fea075e8581c819e6c555e9b8f
Ahnaf Siddiqui <luky1971@users.noreply.github.com>
2016-05-17 17:51:56 -0400

    Added some test file

---
+++ test/plsrun.sh
@@ -0,0 +1,6 @@
+g_correlate -f pdz50ns_nowat.xtc -s pdz50ns.tpr -a 'N H' -nt 1000
+echo 'Diffing unit vectors'
+diff vecs_fbyp.txt NHvecs_fbyp.txt
+diff vecs_pbyf.txt NHvecs_pbyf.txt
+echo 'Diffing autocorrelations'
+diff corr.dat corr_nt1000.dat
